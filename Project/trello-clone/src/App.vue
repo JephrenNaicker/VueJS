81f657bda305a1ca07c9880d57b7d455cf59e92d
Jephren Naicker <Jephren Naicker@DESKTOP-33DTO71>
2025-01-06 20:40:22 +0200

    SetUp Project
    Added Scope Slots and draggable

--- Project/trello-clone/src/App.vue
+++ Project/trello-clone/src/App.vue
@@ -1,47 +1,63 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="p-5 font-sans">
+    <div class="flex gap-5 py-5 overflow-x-auto">
+      <div class="bg-gray-100 p-3 rounded-lg min-w-[250px] flex flex-col" v-for="list in listsArr" :key="list.id">
+        <h2 class="font-medium mb-2">{{ list.title }}</h2>
+      
+        <Draggable :list="list.cards" group="cards">
+          <template #item="{element}">  <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">{{ element.title }}</span>
+          <p class="text-xs text-gray-400">
+            {{ element.description }}
+          </p>
+        </div></template>
+        </Draggable>
+        <button
+          class="w-full bg-transparent rounded hover:bg-white text-gray-500 p-2 text-left mt-2 text-sm font-medium">
+          + Add Card
+        </button>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup lang="ts">
+import { reactive } from 'vue';
+import Draggable from 'vuedraggable';
+
+interface Card {
+  id: number,
+  title: string,
+  description: string
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+interface List {
+  id: number,
+  title: string,
+  cards: Card[]
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+const listsArr = reactive<List[]>([
+  {
+    id: 1,
+    title: 'To Do',
+    cards: [
+      { id: 1, title: 'Task 1', description: 'Description for Task 1' },
+      { id: 2, title: 'Task 2', description: 'Description for Task 2' }
+    ]
+  },
+  {
+    id: 2,
+    title: 'In Progress',
+    cards: [
+      { id: 3, title: 'Task 3', description: 'Description for Task 3' },
+      { id: 4, title: 'Task 4', description: 'Description for Task 4' }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Done',
+    cards: [{ id: 5, title: 'Task 5', description: 'Description for Task 5' }]
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+]);
+</script>
