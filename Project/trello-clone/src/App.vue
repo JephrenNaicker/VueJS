d6877e1a3c88dd7ce2f947504c1b185c41af6e0a
Jephren Naicker <Jephren Naicker@DESKTOP-33DTO71>
2025-01-09 09:41:58 +0200

    edit and add

--- Project/trello-clone/src/App.vue
+++ Project/trello-clone/src/App.vue
@@ -1,42 +1,81 @@
 <template>
   <main class="p-5 font-sans">
     <div class="flex gap-5 py-5 overflow-x-auto">
-      <div class="bg-gray-100 p-3 rounded-lg min-w-[250px] flex flex-col" v-for="list in listsArr" :key="list.id">
+      <div class="bg-gray-100 p-3 rounded-lg min-w-[250px] flex flex-col" v-for="(list, listIndex) in listsArr"
+        :key="list.id">
         <h2 class="font-medium mb-2">{{ list.title }}</h2>
-      
+
         <Draggable :list="list.cards" group="cards" item-key="id ">
-          <template #item="{element}">  <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
-          <span class="text-sm font-medium">{{ element.title }}</span>
-          <p class="text-xs text-gray-400">
-            {{ element.description }}
-          </p>
-        </div></template>
+          <template #item="{ element }">
+            <div class="bg-white p-2 my-2 rounded shadow cursor-pointer" @click="openModal(listIndex,element)">
+              <span class="text-sm font-medium">{{ element.title }}</span>
+              <p class="text-xs text-gray-400">
+                {{ element.description }}
+              </p>
+            </div>
+          </template>
         </Draggable>
         <button
-          class="w-full bg-transparent rounded hover:bg-white text-gray-500 p-2 text-left mt-2 text-sm font-medium" @click="openModal">
+          class="w-full bg-transparent rounded hover:bg-white text-gray-500 p-2 text-left mt-2 text-sm font-medium"
+          @click="openModal(listIndex)">
           + Add Card
         </button>
       </div>
     </div>
-    <ModalDialogue :is-open="isModalOpen" @close="closeModal"></ModalDialogue>
+    <ModalDialogue :is-open="isModalOpen" :card="editCard" :mode="modalMode" @close="closeModal" @save="saveCard">
+    </ModalDialogue>
   </main>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import Draggable from 'vuedraggable';
-import type {List } from '@/types.ts'
+import type { List, Card } from '@/types.ts'
 import ModalDialogue from './components/ModalDialogue.vue';
 
 
 const isModalOpen = ref(false);
+const editCard = ref<Card | null>(null);
+const editListIndex = ref<number | null>(null);
 
-const openModal = ()=>{
+
+const modalMode = computed(() => editCard.value !== null ? 'add' : 'edit');
+
+const openModal = (listIndex: number, card?: Card) => {
+  editListIndex.value = listIndex;
   isModalOpen.value = true;
+
+  editCard.value = card === undefined ? null : card;
 }
 
-const closeModal = ()=>{
+const saveCard = ((card: Card) => {
+  if (editListIndex.value === null) {
+    return;
+  }
+
+  if (modalMode.value === 'add') {
+    //Adding
+    const newId = Math.max(...listsArr.flatMap(list => list.cards.map(c => c.id)));
+    listsArr[editListIndex.value].cards.push({
+      ...card, id: newId
+    })
+  }
+  else {
+    //editing
+    const cardIndex = listsArr[editListIndex.value].cards.findIndex((cardOnList => cardOnList.id === card.id));
+    if (cardIndex !== -1) {
+      listsArr[editListIndex.value].cards[cardIndex] = card;
+    }
+  }
+
+
+  closeModal();
+})
+
+const closeModal = () => {
   isModalOpen.value = false;
+  editListIndex.value = null;
+  editCard.value = null;
 }
 
 const listsArr = reactive<List[]>([
